2dfa61c625687ad04293c2360f7ee60f68805afb
andtyldai <andrewdailey0614@gmail.com>
2025-04-19 01:29:32 -0400

    Now pulls from the email and score tables and writes to the score table

--- emails/email_db.gd
+++ emails/email_db.gd
@@ -1,5 +1,24 @@
 extends Node
 
-## TODO
 func get_email_list()->Array[Email]:
-	return [Email.new()]
+	var db : SQLite = SQLite.new()	
+	db.path = "res://data.db"
+	db.open_db()
+	var emails: Array[Email] = []
+	var result = db.query("SELECT sender, subject, body, is_spam, justification, false_justifications from questions")
+	var results = db.query_result
+	if result:
+		for row in results:
+			var email = Email.new()
+			email.sender = row["sender"]
+			email.subject = row["subject"]
+			email.body = row["body"]		
+			email.is_spam = row["is_spam"]
+			email.justification = row["justification"]
+			email.false_justifications = row["false_justifications"]
+			print(email)
+			emails.append(email)
+	else:
+		print("Query Failed")
+	db.close_db()
+	return emails
--- score/scoreboard.gd
+++ score/scoreboard.gd
@@ -14,6 +14,31 @@ func answer_given(is_correct: bool, is_extra_credit: bool = false)->void:
 	elif !is_correct && !is_extra_credit:
 		current_score.multiplier = 0
 
-## TODO
+func write_score(entry: ScoreEntry)->void:
+	var db : SQLite = SQLite.new()
+	db.path = "res://data.db"
+	db.open_db()
+	var dict = {"username" = entry.username, "score" = entry.score, "multiplier" = entry.multiplier}
+	var result = db.insert_row("scoring", dict)
+	if !result:
+		print("Insert Failed")
+		
+
 func get_scores()->Array[ScoreEntry]:
-	return []
+	var db : SQLite = SQLite.new()
+	db.path = "res://data.db"
+	db.open_db()
+	var scores: Array[ScoreEntry] = []
+	var result = db.query("SELECT username, score, multiplier from scoring")
+	var results = db.query_result
+	if result:
+		for row in results:
+			var score = ScoreEntry.new()
+			score.username = row["username"]
+			score.score = row["score"]
+			score.multiplier = row["multiplier"]
+			scores.append(score)
+	else:
+		print("Query Failed")
+	db.close_db()
+	return scores
